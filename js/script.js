dbfef3be692a1d9bf8b34612de8a5dff971be869
ShanthakumarThanus <thanus.shanthakumar.sio@gmail.com>
2024-03-30 19:28:18 +0100

    dev animation & veille

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form, .headingP', { origin:
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .veille-container, .contact form, .headingP', { origin:
 'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
 ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
